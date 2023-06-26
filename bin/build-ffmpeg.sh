5b6cfaacbea809789a263d2d8bf20766701a9fd7
ranzhouhang <ranzhouhang@xdfzx.com>
2023-06-26 19:38:14 +0800

    feat: add shell

--- bin/build-ffmpeg.sh
+++ bin/build-ffmpeg.sh
@@ -3,10 +3,3 @@
 PATH=$PWD/ffmpeg
 
 $PATH/build.sh
-
-
-# #!/bin/bash -x
-# # "nasm/yasm not found or too old. Use --disable-x86asm for a crippled build."
-# ./configure --prefix=$(dirname $PWD)/ffmlib --enable-static --enable-shared --disable-doc
-# make -j
-# make install
