56ca9df115e52fae68bc3d8920e86fa8b9d12d89
chaxus <nouo18@163.com>
2023-06-28 21:29:33 +0800

    Feat/libavcodec (#2)

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 # "nasm/yasm not found or too old. Use --disable-x86asm for a crippled build."
-./configure --disable-x86asm --prefix=$PWD/dist --enable-static --enable-shared
+./configure --prefix=$(dirname $PWD)/ffmlib --enable-static --enable-shared --disable-doc
 make -j
 make install
